46f10787fcffae7d9e71924cb1b96a56a9fde9a5
JonpeterQ <joleqipi@gmail.com>
2023-10-28 13:53:37 -0500

    feat functions

--- intro.js
+++ intro.js
@@ -23,64 +23,71 @@ let mayoriaedad=18;
 const edadpensionM=62;
 const edadpensionF=57;
 
-if(persona.edad>=edadpensionM && persona.sexo=="M" && persona.semanascot>=edadpensionM){
-    console.log("puede pensionarse");
-}else if(persona.edad>=edadpensionF && persona.sexo=="F" && persona.semanascot>=edadpensionF){
-    console.log("puede pensionarse");
-}else{console.log("no puede pensionarse");}
+function ejemplocondicional(){
+    if(persona.edad>=edadpensionM && persona.sexo=="M" && persona.semanascot>=edadpensionM){
+        console.log("puede pensionarse");
+    }else if(persona.edad>=edadpensionF && persona.sexo=="F" && persona.semanascot>=edadpensionF){
+        console.log("puede pensionarse");
+    }else{console.log("no puede pensionarse");}
+        
+    if(persona.cedula || persona.pasaporte){
+        console.log("puede viajar");
+    }else{
+        console.log("no puede salir del pais");
+    }
     
-if(persona.cedula || persona.pasaporte){
-    console.log("puede viajar");
-}else{
-    console.log("no puede salir del pais");
-}
-
-
-let dia = "Lunes";
-
-switch(dia){
-    case "Lunes":
-        console.log("Hoy es lunes");
-        break;
-    case "Martes":
-        console.log("hoy es martes");
-        break;
-    case "Miercoles":
-        console.log("hoy es miercoles");
-        break;
-    case "Jueves":
-        console.log("Hoy es jueves");
-        break;
-    case "Viernes":
-        console.log("hoy es viernes");
-        break;
-    case "Sabado":
-        console.log("hoy es sabado");
-        break;
-    case "Domingo":
-        console.log("Hoy es domingo");
-        break;
-    default:
-        console.log("dia invalido")
     
+    let dia = "Lunes";
+    
+    switch(dia){
+        case "Lunes":
+            console.log("Hoy es lunes");
+            break;
+        case "Martes":
+            console.log("hoy es martes");
+            break;
+        case "Miercoles":
+            console.log("hoy es miercoles");
+            break;
+        case "Jueves":
+            console.log("Hoy es jueves");
+            break;
+        case "Viernes":
+            console.log("hoy es viernes");
+            break;
+        case "Sabado":
+            console.log("hoy es sabado");
+            break;
+        case "Domingo":
+            console.log("Hoy es domingo");
+            break;
+        default:
+            console.log("dia invalido")
+        
+    }
+    
+    // operador ternario
+    
+    if(persona.sexo=="M"){
+        console.log("Es hombre");
+    }else{
+        console.log("es mujer");
+    }
+    
+    persona.sexo=="M"? console.log("Es un hombre"):console.log("es mujer")
+
 }
 
-// operador ternario
-
-if(persona.sexo=="M"){
-    console.log("Es hombre");
-}else{
-    console.log("es mujer");
+function ejemplociclo(nombres){
+    for(let i=1; i<5;i++){
+        console.log(i+" elefantes se balanceaban sobre la tela de la araña");
+    }
+    
+    for(i = 0; i < nombres.length; i++){
+        console.log(nombres[i]);
+    }
 }
-
-persona.sexo=="M"? console.log("Es un hombre"):console.log("es mujer")
 
 //ciclos
 
-for(let i=1; i<5;i++){
-    console.log(i+" elefantes se balanceaban sobre la tela de la araña");
-}
-
-for(i = 0; i < nombres.length; i++){
-    console.log(nombres[i]);
-}
+ejemplocondicional();
