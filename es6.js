dfce81bda60ff6bfde8798ced522e9d3a58ec435
JonpeterQ <joleqipi@gmail.com>
2023-10-30 08:01:10 -0500

    feat template literal

--- es6.js
+++ es6.js
@@ -1,17 +1,28 @@
 //template literals
 
-function saludar(nombre){
-    return "hola "+ nombre;
+function inicio(){
+    function saludar(nombre){
+        return "hola "+ nombre;
+    }
+
+    
+    function calcularNacimiento(edad){
+        return 2023 - edad;
+    }
+    
+    let nombrePersona = prompt("ingresa tu nombre");
+    let resultado = saludar(nombrePersona);
+    
+    
+    let edadPersona =prompt("Ingresa tu edad");
+    let resultadoCalculo = calcularNacimiento(edadPersona);
+
+    let result = resultado + " tu año de nacimiento es "+ resultadoCalculo;
+    // esto es un template string
+    let result2 =`${resultado} tu año de nacimiento es ${resultadoCalculo}`; 
+    console.log(result2);
+
 }
 
-function calcularNacimiento(edad){
-    return 2023 - edad;
-}
+inicio();
 
-let nombrePersona = prompt("ingresa tu nombre");
-let resultado = saludar(nombrePersona);
-
-
-let edadPersona =prompt("Ingresa tu edad");
-let resultadoCalculo = calcularNacimiento(edadPersona);
-console.log(resultado +" naciste en el año "+ resultadoCalculo);
